cdb96c51eef7d262bd1946baebb8fcce09d91406
Juan Medina - Knowit <juan.medina@knowit.se>
2021-09-29 15:36:25 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,7 @@
 module "s3-bucket_example_object" {
   source  = "app.terraform.io/jmedki-training/s3-bucket/aws//examples/object"
   version = "2.2.0"
-  bucket_prefix = "jmedki"
+  inputs {
+   bucket = "jmedki"
+  }
 }
